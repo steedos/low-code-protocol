fe0c0237d585a6d3e85bdcfda29fe79c3a80dc34
yinlianghui <yinlianghui@hotoa.com>
2022-04-02 23:28:02 +0800

    Update documentation.js

--- src/navs/documentation.js
+++ src/navs/documentation.js
@@ -21,7 +21,7 @@ export const documentationNav = {
   ],
   '软件包开发': [
     pages['objectql'],
-    pages['functions'],
+    // pages['functions'],
     pages['trigger'],
     pages['flow-trigger'],
     pages['services'],

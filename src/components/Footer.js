94e28473b74bb05ccdaaa7d6e81575d62db62cec
庄建国 <50353452+hotlong@users.noreply.github.com>
2022-04-28 14:59:57 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ export function Footer({ children, previous, next }) {
       )}
       <div className="pt-10 pb-28 border-t border-slate-200 sm:flex justify-between text-slate-500 dark:border-slate-200/5">
         <div className="mb-6 sm:mb-0 sm:flex">
-          <p>Copyright &copy; {new Date().getFullYear()} Steedos Inc.</p>
+          <p>Copyright &copy; {new Date().getFullYear()} Low Code Protocol</p>
           {/* <p className="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200 dark:sm:border-slate-200/5">
             <Link href="/brand">
               <a className="hover:text-slate-900 dark:hover:text-slate-400">Trademark Policy</a>
